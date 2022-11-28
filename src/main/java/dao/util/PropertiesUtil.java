bb6dc5cdf21e091e9441c76170ec2cc5c9b307f4
iamusayev <anarmusayev1337@gmail.com>
2022-11-28 16:05:04 +0400

    added connection manager

---
+++ src/main/java/dao/util/PropertiesUtil.java
@@ -0,0 +1,25 @@
+package dao.util;
+
+import java.util.Properties;
+import lombok.SneakyThrows;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class PropertiesUtil {
+    private static final Properties PROPERTIES = new Properties();
+
+    static {
+        loadProperties();
+    }
+
+    public static String get(String key) {
+        return PROPERTIES.getProperty(key);
+    }
+
+    @SneakyThrows
+    private static void loadProperties() {
+        try (var stream = PropertiesUtil.class.getClassLoader().getResourceAsStream("applicatoin.properties")) {
+            PROPERTIES.load(stream);
+        }
+    }
+}
