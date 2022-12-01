4e206d1442792af582af82ad85614dd6171cf8fc
iamusayev <anarmusayev1337@gmail.com>
2022-12-01 20:51:45 +0400

    added connection pool

---
+++ src/main/java/dao/ApplicationRunner.java
@@ -0,0 +1,12 @@
+package dao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationRunner {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationRunner.class, args);
+    }
+}
